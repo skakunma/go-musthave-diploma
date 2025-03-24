88d32fe5c15960bee3d1c8ff02905e8c73903e9b
skakun,a <skakun2025@mail.ru>
2025-03-25 02:16:18 +0300

    New rep

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,10 +1,27 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 func ParseFlags(cfg *Config) {
-	flag.StringVar(&cfg.FlagAddress, "a", ":8000", "address and port to run server")
-	flag.StringVar(&cfg.FlagForDB, "d", "", "database conn link")
-	flag.StringVar(&cfg.FlagAddressAS, "r", "http://localhost:8080", "address accrual system")
-	flag.Parse()
+	// Получаем значения из переменных окружения (если они есть)
+	envAddress := os.Getenv("RUN_ADDRESS")
+	envDB := os.Getenv("DATABASE_URI")
+	envAccrual := os.Getenv("ACCRUAL_SYSTEM_ADDRESS")
+
+	// Устанавливаем флаги с учётом значений из окружения
+	flag.StringVar(&cfg.FlagAddress, "a", envOrDefault(envAddress, ":8000"), "address and port to run server")
+	flag.StringVar(&cfg.FlagForDB, "d", envOrDefault(envDB, ""), "database conn link")
+	flag.StringVar(&cfg.FlagAddressAS, "r", envOrDefault(envAccrual, "http://localhost:8080"), "address accrual system")
+
+	flag.Parse() // Парсим флаги (они могут переопределить значения из окружения)
+}
+
+func envOrDefault(envValue, defaultValue string) string {
+	if envValue != "" {
+		return envValue
+	}
+	return defaultValue
 }
