068636985e3ae1bf807123beda01869369d7e76d
Skakunda honor <skakun2025@mail.ru>
2025-04-08 13:36:58 +0300

    changes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,12 +7,13 @@ import (
 )
 
 func SetupRouter(server *gin.Engine, cfg *config.Config) {
-	server.Use(middleware.AuthMiddleware(cfg))
+	api := server.Group("/api/user")
+	auth := api.Group("", middleware.AuthMiddleware(cfg))
 	server.POST("/api/user/register", func(c *gin.Context) { RegisterHandler(c, cfg) })
 	server.POST("/api/user/login", func(c *gin.Context) { Login(c, cfg) })
-	server.POST("/api/user/orders", func(c *gin.Context) { CreateOrder(c, cfg) })
-	server.GET("/api/user/orders", func(c *gin.Context) { GetOrders(c, cfg) })
-	server.GET("/api/user/balance", func(c *gin.Context) { GetBalance(c, cfg) })
-	server.POST("/api/user/balance/withdraw", func(c *gin.Context) { WithdrawBalance(c, cfg) })
-	server.GET("/api/user/withdrawals", func(c *gin.Context) { GetWithdrawals(c, cfg) })
+	auth.POST("/api/user/orders", func(c *gin.Context) { CreateOrder(c, cfg) })
+	auth.GET("/api/user/orders", func(c *gin.Context) { GetOrders(c, cfg) })
+	auth.GET("/api/user/balance", func(c *gin.Context) { GetBalance(c, cfg) })
+	auth.POST("/api/user/balance/withdraw", func(c *gin.Context) { WithdrawBalance(c, cfg) })
+	auth.GET("/api/user/withdrawals", func(c *gin.Context) { GetWithdrawals(c, cfg) })
 }
