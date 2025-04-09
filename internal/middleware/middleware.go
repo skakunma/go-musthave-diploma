068d90c3d3544861eff675ff3ddefe56600b3aa7
skakun,a <skakun2025@mail.ru>
2025-04-09 12:54:11 +0300

    hz

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -23,6 +23,7 @@ func AuthMiddleware(cfg *config.Config) gin.HandlerFunc {
 		// Проверяем, защищен ли маршрут
 		if _, exist := authPaths[c.Request.URL.Path]; exist {
 			var jwtToken string
+
 			var err error
 
 			// Пробуем взять токен из cookie
